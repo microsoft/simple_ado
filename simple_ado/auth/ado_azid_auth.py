465ca51ec6fe70ca20b10f25d1574bd29230c70d
Alan Matthews <alanmatthews@microsoft.com>
2026-01-21 11:37:18 -0500

    Fix formatting

--- simple_ado/auth/ado_azid_auth.py
+++ simple_ado/auth/ado_azid_auth.py
@@ -11,7 +11,11 @@ class ADOAzIDAuth(ADOAuth):
 
     def __init__(self) -> None:
         # The get_token parameter specifies the Azure DevOps resource and requests a token with default permissions for API access.
-        self.token = DefaultAzureCredential().get_token("499b84ac-1321-427f-aa17-267ca6975798/.default").token
+        self.token = (
+            DefaultAzureCredential()
+            .get_token("499b84ac-1321-427f-aa17-267ca6975798/.default")
+            .token
+        )
 
     def get_authorization_header(self) -> str:
         """Get the header value.
