0df1d1af1807e4dd27ae8999ac805670e84a728b
Alan Matthews <alanmatthews@microsoft.com>
2026-01-21 12:38:09 -0500

    Move token acquisition to get method

--- simple_ado/auth/ado_azid_auth.py
+++ simple_ado/auth/ado_azid_auth.py
@@ -7,20 +7,16 @@ from simple_ado.auth.ado_auth import ADOAuth
 class ADOAzIDAuth(ADOAuth):
     """Azure Identity auth."""
 
-    token: str
-
-    def __init__(self) -> None:
-        # The get_token parameter specifies the Azure DevOps resource and requests a token with
-        # default permissions for API access.
-        self.token = (
-            DefaultAzureCredential()
-            .get_token("499b84ac-1321-427f-aa17-267ca6975798/.default")
-            .token
-        )
-
     def get_authorization_header(self) -> str:
         """Get the header value.
 
         :return: The header value."""
 
-        return "Bearer " + self.token
+        # The get_token parameter specifies the Azure DevOps resource and requests a token with
+        # default permissions for API access.
+        return (
+            "Bearer "
+            + DefaultAzureCredential()
+            .get_token("499b84ac-1321-427f-aa17-267ca6975798/.default")
+            .token
+        )
