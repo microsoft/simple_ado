49941216a9618d4e5ad5e411d045660fca6bb922
Alan Matthews <alanmatthews@microsoft.com>
2026-01-21 11:20:04 -0500

    Add new auth to init.py files

--- simple_ado/auth/__init__.py
+++ simple_ado/auth/__init__.py
@@ -3,10 +3,12 @@
 from .ado_auth import ADOAuth
 from .ado_basic_auth import ADOBasicAuth
 from .ado_token_auth import ADOTokenAuth
+from .ado_azid_auth import ADOAzIDAuth
 
 # Set the module's public interface
 __all__ = [
     "ADOAuth",
     "ADOBasicAuth",
     "ADOTokenAuth",
+    "ADOAzIDAuth",
 ]
