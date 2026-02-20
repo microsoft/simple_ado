e1ef54c5276f23714fc192e39c3ea1b3cd22e302
Alan Matthews <alanmatthews@microsoft.com>
2026-02-20 09:44:55 -0500

    Add buffer to time check

--- simple_ado/auth/ado_azid_auth.py
+++ simple_ado/auth/ado_azid_auth.py
@@ -22,7 +22,7 @@ class ADOAzIDAuth(ADOAuth):
 
         # The get_token parameter specifies the Azure DevOps resource and requests a token with
         # default permissions for API access.
-        if self.access_token is None or self.access_token.expires_on <= time.time():
+        if self.access_token is None or self.access_token.expires_on <= time.time() + 60:
             self.access_token = DefaultAzureCredential().get_token(
                 "499b84ac-1321-427f-aa17-267ca6975798/.default"
             )
